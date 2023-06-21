598d8c7184d3f6a9e573c14713d4413bcdccae9e
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-06-21 12:38:32 +0400

    added assets files

---
+++ src/assets/index.js
@@ -0,0 +1,29 @@
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import Node from "../assets/node.png";
+import React from "../assets/react.png";
+import Mongo from "../assets/mongo.png";
+import Python from "../assets/python.png";
+import Github from "../assets/github.png";
+import Summarify from "../assets/summarify.png";
+import Weather from "../assets/weather.png";
+import Workout from "../assets/workout.png";
+import LeadTracker from "../assets/leadtracker.png";
+import Portfolio from "../assets/portfolio.png";
+
+export {
+  HTML,
+  JavaScript,
+  CSS,
+  Node,
+  Mongo,
+  React,
+  Python,
+  Github,
+  Summarify,
+  Weather,
+  Workout,
+  LeadTracker,
+  Portfolio,
+};
