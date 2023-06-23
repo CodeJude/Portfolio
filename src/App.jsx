344ba1ce072abe33101acf87ec13ab40e31fd123
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-06-23 23:22:09 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./index.css";
+import {
+  Navbar,
+  Hero,
+  About,
+  Skills,
+  Work,
+  Contact,
+  Footer,
+} from "./components";
+const App = () => (
+  <div>
+    <Navbar />
+    <Hero />
+    <About />
+    <Skills />
+    <Work />
+    <Contact />
+    <Footer />
+  </div>
+);
+
+export default App;
