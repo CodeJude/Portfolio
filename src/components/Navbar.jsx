bb65999087d6faf49992a479c4672bdd3bb8c72e
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-06-24 23:41:57 +0400

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,153 @@
+import { useState } from "react";
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { Link } from "react-scroll";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logo from "../assets/logo.png";
+import { JudeCV } from ".";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => {
+    setNav((prevNav) => !prevNav);
+  };
+  // bg-[#0a192f]
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-white ">
+      <div>
+        <img src={Logo} alt="Logo image" style={{ width: "120px" }} />
+      </div>
+      {/* Big Screen Menu */}
+      <ul className="hidden md:flex ">
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* Hamburger Menu */}
+      <div className="md:hidden z-10" onClick={handleClick}>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* Social Icons */}
+      <div className="hidden md:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0079FF]">
+            <a
+              href="https://twitter.com/JudeVector"
+              className="justify-between flex items-center text-gray-300 w-full"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter <FaTwitter size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              href="https://github.com/CodeJude"
+              className="justify-between flex items-center text-gray-300 w-full"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              href="https://www.linkedin.com/in/judendubuisi/"
+              className="justify-between flex items-center text-gray-300 w-full"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+            <a
+              href="mailto: ikechukwujudendubuisi@gmail.com"
+              className="justify-between flex items-center text-gray-300 w-full"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              href={JudeCV}
+              download
+              className="justify-between flex items-center text-gray-300 w-full"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
