1b5b4c7b0bac498402a15b28531581018383dc0f
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-06-25 22:10:36 +0400

    initial commit

---
+++ src/components/WorkCard.jsx
@@ -0,0 +1,30 @@
+const WorkCard = ({ props }) => {
+  return (
+    <div
+      //   onClick={() => window.open({props.path})}
+      style={{ backgroundImage: `url(${props.image})` }}
+      className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+    >
+      {/* Hover Effect */}
+      <div className="opacity-0 group-hover:opacity-100">
+        <span className="text-2xl font-bold text-white tracking-wider ">
+          {props.title}
+        </span>
+        <div className="pt-8 text-center">
+          <a href={props.demo} target="_blank" rel="noreferrer">
+            <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+              Live
+            </button>
+          </a>
+          <a href={props.code} target="_blank" rel="noreferrer">
+            <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+              Code
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkCard;
