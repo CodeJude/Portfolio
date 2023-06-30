151695391e9dba0717ce223170f386977e901e84
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-06-30 15:41:26 +0400

    Add index.js file

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import About from "./About";
+import Skills from "./Skills";
+import Work from "./Work";
+import WorkCard from "./WorkCard";
+import Contact from "./Contact";
+import JudeCV from "./JudeCV.pdf";
+import Footer from "./Footer";
+
+export { Navbar, Hero, About, Skills, Work, WorkCard, Contact, JudeCV, Footer };
