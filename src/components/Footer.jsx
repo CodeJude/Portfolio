6850ba12ad856bdf501b5f5c566450503f4853ef
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-06-30 15:40:43 +0400

    Add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="font-medium flex bg-[#0a192f] text-white w-full justify-center">
+      <p className=" mb-5">
+        Coded with <span className="text-pink-600">❤️</span> by{" "}
+        <button
+          className="text-pink-600"
+          href="https://github.com/CodeJude"
+          onClick={() => window.open("https://github.com/CodeJude")}
+        >
+          CodeJude
+        </button>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
