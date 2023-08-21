09e08c6759527bf0655262434a21efe2a07e7c53
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2023-08-21 22:56:05 +0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ const Footer = () => {
   return (
     <footer className="font-medium flex bg-[#0a192f] text-white w-full justify-center">
       <p className=" mb-5">
-        Coded with <span className="text-pink-600">❤️</span> by{" "}
+        Built with <span className="text-pink-600">❤️</span> by{" "}
         <button
           className="text-pink-600"
           href="https://github.com/CodeJude"
-          onClick={() => window.open("https://github.com/JudeVector")}
+          onClick={() => window.open("https://github.com/judevector")}
         >
-          JudeVector
+          judevector
         </button>
       </p>
     </footer>
